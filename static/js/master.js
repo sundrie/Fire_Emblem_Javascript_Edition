07cc0f771945a9c0e1e122c7e3fbf3bd7c5843ea
unknown <sundrie42@gmail.com>
2018-10-12 21:57:17 +0200

    Ajout d'un rectangle qui me servira de bouton de démarrage

--- static/js/master.js
+++ static/js/master.js
@@ -10,12 +10,26 @@ $(function(){
     var cheight = $("#canvas").height();
     
     function mainMenu(){
-        ctx.fillRectangle
-        ctx.font = '20px Arial';
-        ctx.fillText('Fire Emblem', 420, 50);
-        ctx.font = '16px italic';
-        ctx.fillText('Javascript Edition',420,80);
-    }
+        ctx.font = '60px Arial';
+        ctx.fillText('Fire Emblem', 250, 200);
+        ctx.font = 'italic 20px Arial';
+        ctx.fillText('Javascript Edition',430,230);  
 
+        // Pour plus de simplicité d'accès je stock tout ça sous cette forme
+        var startButton = {
+            x:320,
+            y:270,
+            width:200,
+            height:50
+        }
+        
+        // Dessine un rectangle
+        ctx.strokeRect(startButton.x,startButton.y,startButton.width,startButton.height);
+        
+        // // Pour la suite afin de détecter si le rectangle a été cliqué
+        // $("#canvas").on("click",function(){
+
+        // })
+    }
     mainMenu();
 });

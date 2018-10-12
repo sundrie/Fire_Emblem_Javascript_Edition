b30a61b56a2f1c99ecb57fe1dc069c7643bbbfa3
unknown <sundrie42@gmail.com>
2018-10-12 20:02:31 +0200

    La base du code de phaser est là, pour l'instant un gros rectangle noir s'affiche

---
+++ static/js/master.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 480,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+    var game = new Phaser.Game(config);
+
+    // Charge les images, sons, textures, etc
+    function preload(){
+
+    };
+
+    function create(){
+
+    };
+
+    function update (){
+
+    };
+});

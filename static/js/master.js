958ce441aef74f09c7028ce6ec4ccc219541558f
unknown <sundrie42@gmail.com>
2018-10-12 21:09:17 +0200

    Affichage d'un texte et du css pour essayer de faire le menu de départ

--- static/js/master.js
+++ static/js/master.js
@@ -1,3 +1,21 @@
 $(function(){
+    var canvas = $("#canvas")[0];
+    // Ici je défini la taille pour éviter de mettre ça dans le html
+    canvas.setAttribute('width', 840);
+    canvas.setAttribute('height', 480);
+
+    // Le jeu sera en 2d pas en 3d
+    var ctx = canvas.getContext("2d");
+	var cwidth = $("#canvas").width();
+    var cheight = $("#canvas").height();
     
+    function mainMenu(){
+        ctx.fillRectangle
+        ctx.font = '20px Arial';
+        ctx.fillText('Fire Emblem', 420, 50);
+        ctx.font = '16px italic';
+        ctx.fillText('Javascript Edition',420,80);
+    }
+
+    mainMenu();
 });

0a309f0301951816f246300c09831881fb0e6d2b
unknown <sundrie42@gmail.com>
2018-10-13 22:00:36 +0200

    Ajout de notre objet startButton qui possède une méthode permettant de se générer

--- static/js/master.js
+++ static/js/master.js
@@ -8,21 +8,40 @@ $(function(){
     var ctx = canvas.getContext("2d");
 	var cwidth = $("#canvas").width();
     var cheight = $("#canvas").height();
+
+    // On créé notre objet startingButton
+    var startButton = {
+        x: 320,
+        y : 270,
+        width : 200,
+        height : 50, 
+        color : "#dadadb",
+
+        // méthode pour créer le rectangle
+        drawRect : function() {
+            // Ajoute de la couleur à notre rectangle
+            ctx.fillStyle = this.color;
+            // Dessine un rectangle remplit / strokeRect() au contraire ne dessine que ses bords sans le remplir
+            ctx.fillRect(this.x,this.y,this.width,this.height);
+            
+            // On mets la couleur noire à notre font sinon ça va être de la même couleur que notre rectangle
+            ctx.fillStyle = '#000000';
+            ctx.font = '20px Arial';        
+            // En faisant ainsi c'est à dire à utiliser les valeurs de bases du rectangle la position du texte sera toujours dans celui ci 
+            ctx.fillText('Commencer', this.x+45, this.y+30);
+        }
+    };    
+    
     
     function mainMenu(){
+        
         ctx.font = '60px Arial';
         ctx.fillText('Fire Emblem', 250, 200);
         ctx.font = 'italic 20px Arial';
-        ctx.fillText('Javascript Edition',430,230);  
-
-        // la fontcion drawRect() est une fonction perso qui créé des rectangles
-        var startButton = drawRect(320,270,200,50,"#dadadb");        
-
-        // On mets la couleur noire à notre font sinon ça va être de la même couleur que notre rectangle
-        ctx.fillStyle = '#000000';
-        ctx.font = '20px Arial';        
-        // En faisant ainsi c'est à dire à utiliser les valeurs de bases du rectangle la position du texte sera toujours dans celui ci 
-        ctx.fillText('Commencer', startButton[0]+45, startButton[1]+30);
+        ctx.fillText('Javascript Edition',430,230);         
+        
+        // On appel la méthode de notre objet qi fait un rectangle
+        startButton.drawRect();        
 
         // // Pour la suite afin de détecter si le rectangle a été cliqué
         // $("#canvas").on("click",function(){
@@ -37,27 +56,17 @@ $(function(){
                 x : event.clientX - canvassize.left,
                 y : event.clientY - canvassize.top
             }
-            if (mousetracker.x > startButton.x && mousetracker.x < startButton.x + startButton.width && mousetracker.y > startButton.y && mousetracker.y < startButton.y + startButton.height){
+            if (mousetracker.x > startButton[0] && mousetracker.x < startButton[0] + startButton[2] && mousetracker.y > startButton[1] && mousetracker.y < startButton[1] + startButton[3]){
                 console.log(mousetracker);
                 console.log('survol !');
                 // clearRect() efface le rectangle il suffit d'indiquer les coordonnées qu'on avait rentrée pour créer le rectangle que l'on souhaite maintenant supprimer
-                ctx.clearRect(startButton.x,startButton.y,startButton.width,startButton.height);
+                ctx.clearRect(startButton[0],startButton[1],startButton[2],startButton[3]);
+            } else {
+                // drawRect(startButton[0],startButton[1],startButton[2],startButton[3],startButton[4]);
             }        
         });
         
     }
     mainMenu();
-
-    // Va nous permettre de dessiner des rectangles
-    function drawRect(x,y,width,height,color,) {
-        // Ajoute de la couleur à notre rectangle
-        ctx.fillStyle = color;
-        // Dessine un rectangle remplit / strokeRect() au contraire ne dessine que ses bords sans le remplir
-        ctx.fillRect(x,y,width,height);
-        // On retourne les valeurs utilisées 
-        var retour = [x,y,width,height,color];
-        return retour;
-    }
-
 
 });

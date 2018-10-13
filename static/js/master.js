fa09fc5fbc583afffa08e7a70be86272d64b0157
unknown <sundrie42@gmail.com>
2018-10-13 20:37:47 +0200

    Pour la suite du dev j'ai construit ma propre fonction qui créé des rectangles

--- static/js/master.js
+++ static/js/master.js
@@ -15,24 +15,14 @@ $(function(){
         ctx.font = 'italic 20px Arial';
         ctx.fillText('Javascript Edition',430,230);  
 
-        // Pour plus de simplicité d'accès je stock tout ça sous cette forme
-        var startButton = {
-            x:320,
-            y:270,
-            width:200,
-            height:50
-        }
-
-        // Ajoute de la couleur à notre rectangle
-        ctx.fillStyle = '#dadadb';
-        // Dessine un rectangle remplit / strokeRect() au contraire ne dessine que ses bords sans le remplir
-        ctx.fillRect(startButton.x,startButton.y,startButton.width,startButton.height);
+        // la fontcion drawRect() est une fonction perso qui créé des rectangles
+        var startButton = drawRect(320,270,200,50,"#dadadb");        
 
         // On mets la couleur noire à notre font sinon ça va être de la même couleur que notre rectangle
         ctx.fillStyle = '#000000';
         ctx.font = '20px Arial';        
         // En faisant ainsi c'est à dire à utiliser les valeurs de bases du rectangle la position du texte sera toujours dans celui ci 
-        ctx.fillText('Commencer', startButton.x+45, startButton.y+30);
+        ctx.fillText('Commencer', startButton[0]+45, startButton[1]+30);
 
         // // Pour la suite afin de détecter si le rectangle a été cliqué
         // $("#canvas").on("click",function(){
@@ -49,16 +39,25 @@ $(function(){
             }
             if (mousetracker.x > startButton.x && mousetracker.x < startButton.x + startButton.width && mousetracker.y > startButton.y && mousetracker.y < startButton.y + startButton.height){
                 console.log(mousetracker);
-                console.log('survol !');                  
+                console.log('survol !');
+                // clearRect() efface le rectangle il suffit d'indiquer les coordonnées qu'on avait rentrée pour créer le rectangle que l'on souhaite maintenant supprimer
+                ctx.clearRect(startButton.x,startButton.y,startButton.width,startButton.height);
             }        
         });
         
     }
     mainMenu();
 
-
-
-
+    // Va nous permettre de dessiner des rectangles
+    function drawRect(x,y,width,height,color,) {
+        // Ajoute de la couleur à notre rectangle
+        ctx.fillStyle = color;
+        // Dessine un rectangle remplit / strokeRect() au contraire ne dessine que ses bords sans le remplir
+        ctx.fillRect(x,y,width,height);
+        // On retourne les valeurs utilisées 
+        var retour = [x,y,width,height,color];
+        return retour;
+    }
 
 
 });

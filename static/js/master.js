b70dddc65fca60db6bf02f95b3206b1559133fca
unknown <sundrie42@gmail.com>
2018-10-13 22:36:45 +0200

    Après réflexion il serait mieux d'utiliser un constructeur car il risque d'y avoir d'autres boutons à l'avenir et ça nous sera très utile

--- static/js/master.js
+++ static/js/master.js
@@ -9,29 +9,32 @@ $(function(){
 	var cwidth = $("#canvas").width();
     var cheight = $("#canvas").height();
 
-    // On créé notre objet startingButton
-    var startButton = {
-        x: 320,
-        y : 270,
-        width : 200,
-        height : 50, 
-        color : "#dadadb",
+    
+    // Constructeur de notre objet bouton qui permets de faire de multiples boutons
+    function Button(x, y, width, height,bcolor,tcolor,tfont,text) {
+        this.x = x; 
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.bcolor = bcolor;
+        this.tcolor = tcolor;
+        this.tfont = tfont;
+        this.text = text;
 
-        // méthode pour créer le rectangle
-        drawRect : function() {
+        // méthode pour modéliser le bouton
+        this.drawButton = function() {            
             // Ajoute de la couleur à notre rectangle
-            ctx.fillStyle = this.color;
+            ctx.fillStyle = this.bcolor;
             // Dessine un rectangle remplit / strokeRect() au contraire ne dessine que ses bords sans le remplir
             ctx.fillRect(this.x,this.y,this.width,this.height);
             
             // On mets la couleur noire à notre font sinon ça va être de la même couleur que notre rectangle
-            ctx.fillStyle = '#000000';
-            ctx.font = '20px Arial';        
+            ctx.fillStyle = tcolor;
+            ctx.font = tfont;        
             // En faisant ainsi c'est à dire à utiliser les valeurs de bases du rectangle la position du texte sera toujours dans celui ci 
-            ctx.fillText('Commencer', this.x+45, this.y+30);
+            ctx.fillText(text, this.x+45, this.y+30);
         }
-    };    
-    
+    } 
     
     function mainMenu(){
         
@@ -40,12 +43,14 @@ $(function(){
         ctx.font = 'italic 20px Arial';
         ctx.fillText('Javascript Edition',430,230);         
         
-        // On appel la méthode de notre objet qi fait un rectangle
-        startButton.drawRect();        
+        // On créé une un nouveau bouton en utilisant le constructeur que j'ai fais
+        var startingButton = new Button(320, 270, 200, 50,"#dadadb","#000000","20px Arial","Commencer");
+        // On utilise la méthode pour le générer
+        startingButton.drawButton();
 
         // // Pour la suite afin de détecter si le rectangle a été cliqué
         // $("#canvas").on("click",function(){
-
+        
         // })
 
         // mousemove nous renvoie la position de la souris uniquement quand elle est sur le canvas
@@ -56,13 +61,18 @@ $(function(){
                 x : event.clientX - canvassize.left,
                 y : event.clientY - canvassize.top
             }
-            if (mousetracker.x > startButton[0] && mousetracker.x < startButton[0] + startButton[2] && mousetracker.y > startButton[1] && mousetracker.y < startButton[1] + startButton[3]){
-                console.log(mousetracker);
+            if (mousetracker.x > startButton.x && mousetracker.x < startButton.x + startButton.width && mousetracker.y > startButton.y && mousetracker.y < startButton.y + startButton.height){
+                //console.log(mousetracker);
                 console.log('survol !');
                 // clearRect() efface le rectangle il suffit d'indiquer les coordonnées qu'on avait rentrée pour créer le rectangle que l'on souhaite maintenant supprimer
-                ctx.clearRect(startButton[0],startButton[1],startButton[2],startButton[3]);
+                // ctx.clearRect(startButton.x,startButton.y,startButton.width,startButton.height);
+                // startButton.drawRect();
+                // console.log(startButton.color);
+                // startButton.color="blue"
+                // console.log(startButton.color);
+                
             } else {
-                // drawRect(startButton[0],startButton[1],startButton[2],startButton[3],startButton[4]);
+                // startButton.drawRect();
             }        
         });
         
